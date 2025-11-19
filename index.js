2173b2cc00d98674cf0e83ab77446131182db0da
mohamed mostafa <220081834+mohamedMostafa825@users.noreply.github.com>
2025-11-20 01:18:23 +0200

    use express

--- index.js
+++ index.js
@@ -1,52 +1,101 @@
-const http = require("node:http");
-const fs = require("fs");
-const path = require("path");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("./index.html", "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
-        res.end("error");
-      }
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
-    });
-  } else if (req.url === "/about") {
-    fs.readFile("./about.html", "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
-        res.end("error");
-      }
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
-    });
-  } else if (req.url === "/contact-me") {
-    fs.readFile("./contact-me.html", "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
-        res.end("error");
-      }
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
-    });
-  } else {
-    // Handle other routes or serve 404
-    fs.readFile("./404.html", "utf8", (err, data) => {
-      if (err) {
-        console.log(err);
-        res.end("error");
-      }
-      res.statusCode = 404;
-      res.setHeader("Content-Type", "text/html");
-      res.end(data);
-    });
-  }
+const fs = require("fs");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     fs.readFile("./index.html", "utf8", (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.end("error");
+//       }
+//       res.statusCode = 200;
+//       res.setHeader("Content-Type", "text/html");
+//       res.end(data);
+//     });
+//   } else if (req.url === "/about") {
+//     fs.readFile("./about.html", "utf8", (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.end("error");
+//       }
+//       res.statusCode = 200;
+//       res.setHeader("Content-Type", "text/html");
+//       res.end(data);
+//     });
+//   } else if (req.url === "/contact-me") {
+//     fs.readFile("./contact-me.html", "utf8", (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.end("error");
+//       }
+//       res.statusCode = 200;
+//       res.setHeader("Content-Type", "text/html");
+//       res.end(data);
+//     });
+//   } else {
+//     // Handle other routes or serve 404
+//     fs.readFile("./404.html", "utf8", (err, data) => {
+//       if (err) {
+//         console.log(err);
+//         res.end("error");
+//       }
+//       res.statusCode = 404;
+//       res.setHeader("Content-Type", "text/html");
+//       res.end(data);
+//     });
+//   }
+// });
+
+app.get("/", (req, res) => {
+  fs.readFile("./index.html", (err, data) => {
+    if (err) {
+      res.end(err);
+    }
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.send(data);
+  });
 });
 
-server.listen(8080, () => {
-  console.log(`Server is running`);
+app.get("/about", (req, res) => {
+  fs.readFile("./about.html", (err, data) => {
+    if (err) {
+      res.end(err);
+    }
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.send(data);
+  });
+});
+
+app.get("/contact-me", (req, res) => {
+  fs.readFile("./contact-me.html", (err, data) => {
+    if (err) {
+      res.end(err);
+    }
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.send(data);
+  });
+});
+
+app.get("/{*splat}", (req, res) => {
+  fs.readFile("./404.html", (err, data) => {
+    if (err) {
+      res.end(err);
+    }
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.send(data);
+  });
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`listening on port ${PORT}`);
 });
